ee69550857f58373b641ac3042d77a64f9a5d6b0
oxy <90028102+7j6@users.noreply.github.com>
2022-11-11 01:48:49 -0500

    Update monkesexsupport.lua

--- monkesexsupport.lua
+++ monkesexsupport.lua
@@ -38,13 +38,13 @@ getgenv().Aiming = {
     Enabled = true,
 
     ShowFOV = false,
-    FOV = 20,
+    FOV = 16,
     FOVSides = 25,
     FOVColour = Color3fromRGB(255, 255, 255),
 
     VisibleCheck = true,
     
-    HitChance = 85,
+    HitChance = 100,
 
     Selected = nil,
     SelectedPart = nil,
