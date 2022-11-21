87cfdab3b384af5d0ad5f61829fdbc5db5fa94d5
oxy <90028102+7j6@users.noreply.github.com>
2022-11-20 22:49:49 -0500

    Update monkeysex.lua

--- monkeysex.lua
+++ monkeysex.lua
@@ -15,7 +15,7 @@
                             local DaHoodSettings = {
                                 SilentAim = true,
                                 AimLock = false,
-                                Prediction = 0.151,
+                                Prediction = 0.1377,
                                 AimLockKeybind = Enum.KeyCode.E
                             }
                             getgenv().DaHoodSettings = DaHoodSettings
